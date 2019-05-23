bd7a629760e2ccbaf1d649dfe3f0a5d41cd3c176
tightcode <tightcodedev@gmail.com>
2019-05-23 05:39:21 -0700

    0.2.3 update

---
+++ zennav.js
@@ -0,0 +1,49 @@
+/* Slidenav 4.0.0-alpha.1 ( https://github.com/tightcode/slidenav ) */
+
+// CSS media feature is used to detect if the user has requested that the system minimize the amount of animation or motion it uses
+var mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+var slideSpeed = 350;
+
+var slideSpeedInit = function () {
+    if (mediaQuery.matches) {
+        slideSpeed = 1;
+    } else {
+        slideSpeed = 350;
+    }
+};
+
+window.addEventListener("load", slideSpeedInit);
+mediaQuery.addEventListener("change", slideSpeedInit);
+
+// Jquery slide effect
+$('.jq-slide').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideSpeed);
+});
+$('.jq-slide').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideSpeed);
+});
+
+// JqueryUI slide effect
+$('.jqui-slide').on('show.bs.dropdown', function() {
+  if (window.matchMedia("(max-width: 991.98px)").matches) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideSpeed);
+  } else {
+    $(this).find('.dropdown-menu').first().stop(true, true).show("slide", {
+      direction: 'up'
+    }, slideSpeed);
+  }
+});
+$('.jqui-slide').on('hide.bs.dropdown', function() {
+  if (window.matchMedia("(max-width: 991.98px)").matches) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideSpeed);
+  } else {
+    $(this).find('.dropdown-menu').first().stop(true, true).hide("slide", {
+      direction: 'up'
+    }, slideSpeed);
+  }
+});
+
+
+
+/* Zennav 4.0.0-alpha ( https://github.com/tightcode/zennav ) */
+// changed breakpoint from md to lg in Slidenav
